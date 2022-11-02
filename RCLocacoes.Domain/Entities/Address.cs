0c8de54d166e99a29bb677faf288d6d94109524e
Ricardo Maciel <42289437+Ricardudles@users.noreply.github.com>
2022-11-02 00:28:23 -0300

    more entities

---
+++ RCLocacoes.Domain/Entities/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCLocacoes.Domain.Entities
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public int ZipCode { get; set; }
+        public char Country { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string AdditionalDetails { get; set; }
+    }
+}
---
+++ RCLocacoes.Domain/Entities/Local.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCLocacoes.Domain.Entities
+{
+    public class Local
+    {
+        public int Id { get; set; }
+        public int AddressId { get; set; }
+        public string Name { get; set; }
+        public string Note { get; set; }
+        public string Contact { get; set; }
+
+        public virtual Address Address { get; set; }
+    }
+}
