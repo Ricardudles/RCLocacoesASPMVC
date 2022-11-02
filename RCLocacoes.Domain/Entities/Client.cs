11254d92c5b316b35327c0f60d83219372dc6168
Ricardo Maciel <42289437+Ricardudles@users.noreply.github.com>
2022-11-02 00:02:51 -0300

    Add project files.

---
+++ RCLocacoes.Domain/Entities/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCLocacoes.Domain.Entities
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public char Nationality { get; set; }
+        public string? Document { get; set; }
+        public string? Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+
+    }
+}
